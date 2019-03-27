b73ced9d011c7ec90c8c88feed4d8cd0346065ba
Mario Tavarez <mtavarez@uelit.com.mx>
2019-03-26 18:07:18 -0600

    Entrega de información Detalle Sucursal
    Se hace la entrega de información mediante formgroup para detalle sucursal

--- src/app/views/sucursales/sucursal-detalle/sucursal-detalle.component.ts
+++ src/app/views/sucursales/sucursal-detalle/sucursal-detalle.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {ModalDirective} from 'ngx-bootstrap/modal';
+import { ModalDirective } from 'ngx-bootstrap/modal';
 
 // Navigation
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
+// Forms
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+// Interfaces
+import { SucursalesInterface } from '../../../interfaces/sucursales.interface';
 
 @Component({
   selector: 'app-sucursal-detalle',
@@ -14,10 +20,63 @@ export class SucursalDetalleComponent implements OnInit {
   @ViewChild('myModal') public myModal: ModalDirective;
   @ViewChild('sucursalModal') public perfilModal: ModalDirective;
 
-  constructor( private activeRoute: ActivatedRoute ) { }
+  // Sucursal Interface que se utolizara para el biding del formulario
+  public sucursal: SucursalesInterface = {
+    id$: '',
+    nombre: '',
+    telefono: '',
+    correo: '',
+    status: '',
+    responsable: '',
+    direccion: ''
+  };
+
+  // Para realizar las validaciones del formulario de sucursales
+  formSucursales: FormGroup;
+
+  // Para realizar las validaciones del formulario modal de sucursales
+  formSucursalesModal: FormGroup;
+
+  constructor(private activeRoute: ActivatedRoute) {
+
+    this.formSucursales = new FormGroup({
+      'id$': new FormControl(''),
+      'nombre': new FormControl(''),
+      'telefono': new FormControl(''),
+      'correo': new FormControl(''),
+      'status': new FormControl(''),
+      'responsable': new FormControl(''),
+      'direccion': new FormControl(''),
+    });
+
+    this.formSucursalesModal = new FormGroup({
+      'id$': new FormControl(''),
+      'nombre': new FormControl(''),
+      'telefono': new FormControl(''),
+      'correo': new FormControl(''),
+      'status': new FormControl(''),
+      'responsable': new FormControl(''),
+      'direccion': new FormControl(''),
+    });
+
+  }
 
   ngOnInit() {
-    console.log(this.activeRoute.snapshot.paramMap.get('id'));
+    // console.log(this.activeRoute.snapshot.paramMap.get('id'));
+    this.formSucursales.valueChanges.subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+    this.formSucursalesModal.valueChanges.subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
+
+  guardaSucursalModal() {
+    console.log(this.formSucursalesModal.value);
   }
 
 }
